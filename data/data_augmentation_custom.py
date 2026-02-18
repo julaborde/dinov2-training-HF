06e1684c78542d6f1b1983f61c0f65f262ea18a8
Julien Laborde-Peyré <julien.lbrd@gmail.com>
2026-02-18 10:59:10 -0500

    Aug + config

--- data/data_augmentation_custom.py
+++ data/data_augmentation_custom.py
@@ -1,5 +1,5 @@
 from torchvision import transforms
-from utils.dino_utils import GaussianBlur, Solarization
+from utils.dino_utils import GaussianBlur
 from torchvision.transforms.functional import InterpolationMode
 import torch 
 
@@ -16,47 +16,64 @@ class ZScoreNormalize:
 
 class DataAugmentationDINO(object):
     def __init__(self, cfg):
+
+        
         local_crops_number = cfg.crops.local_crops_number
         local_crops_scale = cfg.crops.local_crops_scale
         global_crops_scale = cfg.crops.global_crops_scale
         local_crops_size = cfg.crops.local_crops_size
         global_crops_size = cfg.crops.global_crops_size
         self.local_crops_number = local_crops_number
-        flip_and_color_jitter = transforms.Compose([
-            transforms.RandomApply(
-                [transforms.ColorJitter(brightness=0.4, contrast=0.4, saturation=0.2, hue=0.1)],
-                p=0.8
-            ),
-        ])
-        
-        normalize = transforms.Compose([
-            transforms.ToTensor(),
-            ZScoreNormalize()
-            ])
-        
+
+
+        # flip_and_color_jitter = transforms.Compose([
+        #     transforms.RandomApply(
+        #         [transforms.ColorJitter(brightness=0.4, contrast=0.4, saturation=0.2, hue=0.1)],
+        #         p=0.8
+        #     ),
+        # ])
+
+        # intensity = transforms.Compose([
+        #     transforms.RandomApply(
+        #         [transforms.ColorJitter(brightness=0.2, contrast=0.2)], 
+        #         p=0.8
+        #     ),
+        #     transforms.RandomApply(
+        #         [transforms.RandomGamma(gamma=(0.7, 1.5))] if hasattr(transforms, "RandomGamma") else [],
+        #         p=0.3
+        #     ),
+        # ])
+
+
+
         # first global crop
         self.global_transfo1 = transforms.Compose([
             transforms.RandomResizedCrop(global_crops_size, scale=global_crops_scale,ratio=(1.0, 1.0), interpolation=InterpolationMode.BICUBIC),
-            flip_and_color_jitter,
+            
             GaussianBlur(1.0),
-            normalize,
+            transforms.ToTensor(),
+            ZScoreNormalize()
+            
         ])
         
         # second global crop
         self.global_transfo2 = transforms.Compose([
             transforms.RandomResizedCrop(global_crops_size, scale=global_crops_scale, ratio=(1.0, 1.0), interpolation=InterpolationMode.BICUBIC),
-            flip_and_color_jitter,
+            
             GaussianBlur(0.1),
-            Solarization(0.2),
-            normalize,
+            transforms.ToTensor(),
+            ZScoreNormalize()
         ])
         
         # transformation for the local small crops
         self.local_transfo = transforms.Compose([
             transforms.RandomResizedCrop(local_crops_size, scale=local_crops_scale, ratio=(1.0, 1.0), interpolation=InterpolationMode.BICUBIC),
-            flip_and_color_jitter,
+            #transforms.RandomRotation(degrees=10),
+            
             GaussianBlur(p=0.5),
-            normalize,
+            transforms.ToTensor(),
+            ZScoreNormalize()
+            
         ])
     
     def __call__(self, image):
